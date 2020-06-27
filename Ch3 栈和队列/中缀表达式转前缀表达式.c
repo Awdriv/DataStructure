8cc91bd095bb015537aaba145763251f7a39b16d
Awdriv <awdriv@outlook.com>
2020-06-27 17:05:21 +0800

    Updated.

---
+++ Ch3 栈和队列/中缀表达式转前缀表达式.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int getPriority(char op) {
+    if (op == '+' || op == '-') {
+        return 1;
+    } else if (op == '*' || op == '/') {
+        return 2;
+    } else {
+        return 0;
+    }
+}
+
+void convert(char infix[], char opt[], int *top) {
+    int i = strlen(infix);
+    char tmp[233];
+    int pTmp = 0;
+    char ch = 0;
+    while (i--) {
+        ch = infix[i];
+        if (ch >= 'a' && ch <= 'z') {
+            opt[(*top)++] = ch;
+        } else if (ch == ')') {
+            tmp[pTmp++] = ')';
+        } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+            if (tmp[pTmp - 1] != '(') {
+                while (pTmp && getPriority(ch) < getPriority(tmp[pTmp - 1])) {
+                    opt[(*top)++] = tmp[--pTmp];
+                }
+            }
+            tmp[pTmp++] = ch;
+        } else if (ch == '(') {
+            char tch;
+            while ((tch = tmp[--pTmp]) != ')') {
+                opt[(*top)++] = tch;
+            }  
+        }
+    }
+    while (pTmp) {
+        opt[(*top)++] = tmp[--pTmp];
+    }
+}
+
+int main() {
+    int top = 0;
+    char ifx[666], opt[233];
+    
+    scanf("%s\n", ifx);
+    convert(ifx, opt, &top);
+    
+    for (int i = top - 1; i >= 0; i--) {
+        printf("%c", opt[i]);
+    }
+    
+    return 0;
+}
---
+++ Ch3 栈和队列/中缀表达式转后缀表达式.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int getPriority(char op) {
+    if (op == '+' || op == '-') {
+        return 1;
+    } else if (op == '*' || op == '/') {
+        return 2;
+    } else {
+        return 0;
+    }
+}
+
+void convert(char infix[], char opt[], int *top) {
+    int i = 0;
+    char tmp[233];
+    int pTmp = 0;
+    char ch = 0;
+    while (ch = infix[i++]) {
+        if (ch >= 'a' && ch <= 'z') {
+            opt[(*top)++] = ch;
+        } else if (ch == '(') {
+            tmp[pTmp++] = '(';
+        } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+            if (tmp[pTmp - 1] != '(') {
+                while (pTmp && getPriority(ch) <= getPriority(tmp[pTmp - 1])) {
+                    opt[(*top)++] = tmp[--pTmp];
+                }
+            }
+            tmp[pTmp++] = ch;
+        } else if (ch == ')') {
+            char tch;
+            while ((tch = tmp[--pTmp]) != '(') {
+                opt[(*top)++] = tch;
+            }  
+        }
+    }
+    while (pTmp) {
+        opt[(*top)++] = tmp[--pTmp];
+    }
+}
+
+int main() {
+    char ifx[666];
+    char opt[233];
+    int top = 0;
+    
+    scanf("%s\n", ifx);
+    convert(ifx, opt, &top);
+    opt[top] = 0;
+    printf("%s\n", opt);
+    return 0;
+}
